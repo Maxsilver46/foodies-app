8c7131cc73e6b7c6dd81e502e6acfe2d0c6bf256
Maxsilver46 <142869326+Maxsilver46@users.noreply.github.com>
2025-11-11 12:09:19 +0530

    Add styles to fix alignment of meal items

--- components/meals/mealsGrid.js
+++ components/meals/mealsGrid.js
@@ -2,7 +2,7 @@ import MealItem from "./mealItem";
 
 export default function MealsGrid({ meals }) {
   return (
-    <ul>
+    <ul className="w-[90%] max-w-[90rem] grid grid-cols-[repeat(auto-fill,minmax(20rem,1fr))] gap-20 my-8 mx-auto list-none p-0">
       {meals.map((meal) => (
         <li key={meal.id} className="meal-item">
           <MealItem {...meal} />
